1ba03d39adc29837b4cca693434aa507985e9c51
Mag <166319982+magali-la@users.noreply.github.com>
2026-05-20 12:33:16 -0400

    feat(fav word): add script to take a user's favorite word and manipulate it

---
+++ string_fun.py
@@ -0,0 +1,10 @@
+# bonus strings practice in python - script takes favorite word
+user_input = input("Share your favorite word: ")
+print(f"Your favorite word is: {user_input}")
+
+# length of word
+print(f"Your favorite word is {len(user_input)} letters long")
+# word in caps
+print(f"Your favorite word in CAPS: {user_input.upper()}")
+# word 3 times
+print(f"Your favorite word 3 times: {user_input * 3}")
