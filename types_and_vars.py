865546c51ce4229d58d2c75fb71b8942bd1e8ef4
Mag <166319982+magali-la@users.noreply.github.com>
2026-05-19 21:40:27 -0400

    feat(profile): add calculations for future age, dog years, rectangle, and estate area with floating and rounding

--- types_and_vars.py
+++ types_and_vars.py
@@ -1,3 +1,4 @@
+# This script prints info about Cruella de Vil and calculates how many acres her estate is.
 # profile variables
 name = "Cruella de Vil"
 age = 40
@@ -5,3 +6,21 @@ height = 1.85
 
 # print the person
 print(f"Hello, my name is {name}. I am {age} years old and {height} meters tall.")
+
+# calculate age in 5 years
+print("In 5 years " + f"I will be {age + 5} years old.")
+
+# calculate age in dog years and limit to 1 decimal place
+dogAge = age / 7 # full number is 5.714285714285714
+print(f"If I were a Dalmatian, I would be about {dogAge:.1f} years old.")
+
+# calculate area of a rectangle
+width = 5.5
+height = 2
+print(f"The area of a rectangle a {width} x {height} rectangle is {width * height}.")
+
+# calculates the amount of land Cruella has - format sqMiles to two decimal places and round up to nearest integer for acres
+estateWidth = 1.25
+estateLength = 2.20
+estateSqMiles = estateWidth * estateLength
+print(f"My estate is {estateWidth} miles wide and {estateLength} miles long. This is a total of {estateSqMiles:.2f} square miles. I have {round(estateSqMiles * 640)} acres of land.")
